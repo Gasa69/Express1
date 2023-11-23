4dcb6d81d8dde1fb1ea0d31c2a30405b30a86be6
Leonardo G <leonardo.gasparic12@gmail.com>
2023-11-23 10:45:01 +0100

    Dodan tailwind i daisyui

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/signin', function(req, res, next) {
+  res.render('users/signin');
 });
 
 module.exports = router;
